755d03c9011c28f90b3515746d28ae9de5e9500e
tralki <toradoraslol@gmail.com>
2023-11-19 19:33:10 +0000

    BackEnd Update
    the backend server can now be launched in to the localhost , the only thing that sould be touched for now is the main the rest is all automaticly generated

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,20 @@
+
+#C:\Users\torad\Documents\python311\python.exe -m uvicorn main:app --reload    computador Rafael
+
 from typing import Union
 
 from fastapi import FastAPI
 
+
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/exemplo")
+def exemplo()-> str:
+    return "Hello World"
 
-
+#if __name__ == "__name__":
+ #   uvicorn.run(app, port=8000)
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
