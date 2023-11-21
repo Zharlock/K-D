1cb45e492ee00780e4169ba004fb5e4fd0471add
tralki <toradoraslol@gmail.com>
2023-11-21 17:58:31 +0000

    [BackEnd] - CRUD changes
    The backend team gathered in discord and made the following changes:
    Main.py:
    Changes to make it work with "costumer" table
    
    Customer.py (Name subject to changes)
    Added the CRUD for each table of the DB

--- backend/dependensies.py
+++ backend/dependensies.py
@@ -1,3 +1,3 @@
-# pip isntall fastapi[all]
-# pip install "uvicorn[standard]"
-# pip install psycopg2
+#python.exe -m pip install fastapi[all]
+#python.exe -m pip install "uvicorn[standard]"
+#python.exe -m pip install psycopg2
--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,47 @@
 
-#C:\Users\torad\Documents\python311\python.exe -m uvicorn main:app --reload    computador Rafael
-#C:\Users\torad\Documents\python311\python.exe -m uvicorn Customer:app --reload 
-
+from Customer import *
+from Customer import CustomerResponse 
 from typing import Union
-
 from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends, Response
+from fastapi.openapi.models import Response, Info, ExternalDocumentation
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, DateTime 
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.orm import Session
+from Customer import (
+    create_customer,
+    read_customer,
+    update_customer,
+    delete_customer,
+    CustomerResponse,
+    CustomerCreate,
+    CustomerUpdate,
+    ResponseModel,
+    get_db,
+)
 
-
+#****************************************************************************************************
+#C:\Users\torad\Documents\python311\python.exe -m uvicorn main:app --reload    computador Rafael
+#C:\Users\torad\Documents\python311\python.exe -m uvicorn Customer:app --reload    computador Rafael
+#E:\Programming\Python\Python312\python.exe -m uvicorn main:app --reload    computador Carlos
+#E:\Programming\Python\Python312\python.exe -m uvicorn Customer:app --reload    computador Carlos
+#C:\Python\python311\python.exe -m uvicorn main:app --reload    computador Rodrigo
+#C:\Python\python311\Python312\python.exe -m uvicorn Customer:app --reload    computador Rodrigo
+#****************************************************************************************************
+'''
 app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/exemplo")
@@ -19,3 +53,40 @@ def exemplo()-> str:
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/customers/{customer_id}", response_model=CustomerResponse)
+def re_customer(customer_id: int, db: Session = Depends(get_db)):
+    read_customer(customer_id , db )
+    return HTTPException(status_code=404, detail="Customer not found")
+'''
+#*******************************************************
+
+
+app = FastAPI()
+
+@app.get("/exemplo")
+def exemplo() -> str:
+    return "Hello World"
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+# CRUD Operations for Customer
+@app.post("/customers/", response_model=ResponseModel)
+def create_customer_route(customer: CustomerCreate, db: Session = Depends(get_db)):
+    return create_customer(customer, db)
+
+@app.get("/customers/{customer_id}", response_model=CustomerResponse)
+def read_customer_route(customer_id: int, db: Session = Depends(get_db)):
+    return read_customer(customer_id, db)
+
+@app.put("/customers/{customer_id}", response_model=ResponseModel)
+def update_customer_route(
+    customer_id: int, customer_update: CustomerUpdate, db: Session = Depends(get_db)
+):
+    return update_customer(customer_id, customer_update, db)
+
+@app.delete("/customers/{customer_id}", response_model=ResponseModel)
+def delete_customer_route(customer_id: int, db: Session = Depends(get_db)):
+    return delete_customer(customer_id, db)
